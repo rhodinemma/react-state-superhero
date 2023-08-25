48af6b2ea062fd38146902a9cb9ca76be85955ee
rhodinemma <rhodinemma10@gmail.com>
2023-08-25 17:07:40 +0300

    feat: modify tasklist section and add styles for single task

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,5 +1,34 @@
+import { FiEdit3 } from "react-icons/fi";
+import { CiTrash } from "react-icons/ci";
+import {
+  MdOutlineRadioButtonUnchecked,
+  MdOutlineRadioButtonChecked,
+  MdOutlineSaveAs,
+} from "react-icons/md";
+
 const TaskList = () => {
-  return <div className="taskListContainer">TaskList</div>;
+  return (
+    <div className="taskListContainer">
+      <div className="task">
+        <MdOutlineRadioButtonUnchecked />
+        <p>Sample task</p>
+        <FiEdit3 />
+        <CiTrash />
+      </div>
+      <div className="task">
+        <MdOutlineRadioButtonUnchecked />
+        <p>Sample task</p>
+        <FiEdit3 />
+        <CiTrash />
+      </div>
+      <div className="task">
+        <MdOutlineRadioButtonUnchecked />
+        <p>Sample task</p>
+        <FiEdit3 />
+        <CiTrash />
+      </div>
+    </div>
+  );
 };
 
 export default TaskList;
